152fcbd412bef51a58842ee6ae6ee72d499df229
Mark-DSouza <markdsouza434@gmail.com>
2022-12-29 17:03:52 +0530

    Add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,46 @@
-// export default function Home() {
-//   return (
-//     <div className="flex h-screen w-screen flex-col items-center justify-center">
-//       <div className="text-center text-2xl">Which pokémon is rounder?</div>
-//       <div className="p-2" />
-//       <div className="flex max-w-2xl items-center justify-between rounded border p-8">
-//         <div className="h-16 w-16 bg-pink-400"></div>
-//         <div className="p-8">VS</div>
-//         <div className="h-16 w-16 bg-emerald-300"></div>
-//       </div>
-//     </div>
-//   );
-// }
+import { getOptionsForVote } from "../utils/getRandomPokemon";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-import { trpc } from "../utils/trpc";
-
-export default function IndexPage() {
-  const hello = trpc.hello.useQuery({ text: "Mark" });
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
+export default function Home({
+  first,
+  second,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div>
-      <p>{hello.data.greeting}</p>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="text-center text-2xl">Which pokémon is rounder?</div>
+      <div className="p-2" />
+      <div className="flex max-w-2xl items-center justify-between rounded border p-8">
+        <div className="h-16 w-16 bg-pink-400">{first}</div>
+        <div className="p-8">VS</div>
+        <div className="h-16 w-16 bg-emerald-300">{second}</div>
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  first: number;
+  second: number;
+}> = async () => {
+  const [first, second] = getOptionsForVote();
+  return {
+    props: {
+      first,
+      second,
+    }, // will be passed to the page component as props
+  };
+};
+
+// import { trpc } from "../utils/trpc";
+
+// export default function IndexPage() {
+//   const hello = trpc.hello.useQuery({ text: "Mark" });
+//   if (!hello.data) {
+//     return <div>Loading...</div>;
+//   }
+//   return (
+//     <div>
+//       <p>{hello.data.greeting}</p>
+//     </div>
+//   );
+// }
